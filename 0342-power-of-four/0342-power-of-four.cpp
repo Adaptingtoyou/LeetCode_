4f2e9b0de70cedb4d82cf8cd35d0f3db49ead58c
Your Superior <anitarawat1718@gmail.com>
2026-07-26 21:28:41 +0530

    Time: 0 ms (100%), Space: 7.8 MB (81.18%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool power(int n){
+        if(n == 1){
+            return true;
+        }
+        if(n%4 != 0){
+            return false;
+        }
+        return power(n/4);
+    }
+    bool isPowerOfFour(int n) {
+        if(n==0){
+            return false;
+        }
+        if( n == -1){
+            return false;
+        }
+        return power(n);
+    }
+};
